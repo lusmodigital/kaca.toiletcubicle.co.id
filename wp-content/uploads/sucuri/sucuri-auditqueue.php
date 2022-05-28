85b8c73567e848b531effaac0adfaf556ffeeb70
Syamsul Alam <admin@alampintar.org>
2022-05-28 13:01:35 +0700

    update wp-content dengan konten terbaru

---
+++ wp-content/uploads/sucuri/sucuri-auditqueue.php
@@ -0,0 +1,7 @@
+<?php
+// datastore=auditqueue;
+// created_on=1576633204;
+// updated_on=1576633204;
+exit(0);
+?>
+1576633204_9602:"Debug: Syamsul Alam, 139.195.75.226; Sucuri plugin has been uninstalled"
---
+++ wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1576633204;
+// updated_on=1576633204;
+exit(0);
+?>
